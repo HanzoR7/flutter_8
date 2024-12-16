4a144bf43d6d99b08d7e5b7caf8445b940c47adb
HanzoR7 <marat.kalagovr6s@gmail.com>
2024-12-16 14:21:03 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:pr8/Pages/FavouritePage.dart';
+import 'package:pr8/Pages/ItemsPage.dart';
+import 'package:pr8/Pages/ProfilePage.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:pr8/Pages/ShopCartPage.dart';
+import 'package:pr8/api_service.dart';
+import 'package:pr8/model/items.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widgetOptions = [
+      ItemsPage(navToShopCart: (i) => onTab(i)),
+      FavoritePage(navToShopCart: (i) => onTab(i)),
+      ShopCartPage(navToShopCart: (i) => onTab(i)),
+      const ProfilePage()
+    ];
+  }
+  /*
+  int count = ShoppingCart.fold(0, (sum, item) => sum + item.count);
+
+  void updateCount() {
+    setState(() {
+      count = ShoppingCart.fold(0, (sum, item) => sum + item.count);
+    });
+  }
+  */
+
+  void onTab(int i) {
+    setState(() {
+      selectedIndex = i;
+    });
+  }
+
+  static List<Widget> widgetOptions = <Widget>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetOptions.elementAt(selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Главная',
+              backgroundColor: Color.fromRGBO(255, 255, 255, 1)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Избранное',
+              backgroundColor: Color.fromRGBO(255, 255, 255, 1)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_rounded),
+              label: 'Корзина',
+              backgroundColor: Color.fromRGBO(255, 255, 255, 1)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Профиль',
+              backgroundColor: Color.fromRGBO(255, 255, 255, 1))
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: const Color.fromARGB(255, 0, 0, 0),
+        unselectedItemColor: const Color.fromARGB(255, 118, 108, 81),
+        onTap: onTab,
+      ),
+    );
+  }
+}
